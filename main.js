19019cf454cb8d0b37e9313201c4cf417787b501
Vladimir Andrejev <andrejevve@gmail.com>
2022-05-30 22:16:38 +0300

    fix

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ class Main {
 
    open() {
       var doors = document.getElementsByClassName('preview');
-      console.log(2);
       Array.from(doors).forEach((element) => element.classList.add('open'));
    }
 }
